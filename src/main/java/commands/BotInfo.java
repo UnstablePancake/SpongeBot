3370fdae8e70c50728ee2afbc5a3ec83b681d767
Richard <pxdp4r0x@gmail.com>
2016-11-14 20:12:13 -0800

    added status changer

--- src/main/java/commands/BotInfo.java
+++ src/main/java/commands/BotInfo.java
@@ -1,17 +1,27 @@
 package commands;
 
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.Status;
 import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.util.MissingPermissionsException;
 import sx.blah.discord.util.RateLimitException;
 
 public class BotInfo {
 
-    public static void nameChanger (MessageReceivedEvent event) throws RateLimitException, DiscordException, MissingPermissionsException {
+    public static void botInfoChanger (MessageReceivedEvent event) throws RateLimitException, DiscordException, MissingPermissionsException {
         if ((event.getMessage().getAuthor().getID().equals("164909448043823104")) && event.getMessage().toString().startsWith(".setname ")) {
             String newName = event.getMessage().toString().substring(8);
-
             event.getClient().changeUsername(newName);
+        }
+        if ((event.getMessage().getAuthor().getID().equals("164909448043823104")) && event.getMessage().toString().startsWith(".resetname")) {
+            event.getClient().changeUsername("Sponge");
+        }
+        if ((event.getMessage().getAuthor().getID().equals("164909448043823104")) && event.getMessage().toString().startsWith(".setstatus ")) {
+            String newTopic = event.getMessage().toString().substring(11);
+            event.getClient().changeStatus(Status.game(newTopic));
+        }
+        if ((event.getMessage().getAuthor().getID().equals("164909448043823104")) && event.getMessage().toString().startsWith(".resetstatus")) {
+            event.getClient().changeStatus(Status.game(null));
         }
     }
 }
