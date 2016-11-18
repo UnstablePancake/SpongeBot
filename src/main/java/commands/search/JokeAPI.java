0a57056ffa36650cef8c05cec4acd8da684d341e
Richard <pxdp4r0x@gmail.com>
2016-11-17 21:35:37 -0800

    initial commit

---
+++ src/main/java/commands/search/JokeAPI.java
@@ -0,0 +1,13 @@
+package commands.search;
+
+public class JokeAPI {
+    private String joke;
+
+    public String getJoke() {
+        return joke;
+    }
+
+    public void setJoke(String joke) {
+        this.joke = joke;
+    }
+}
