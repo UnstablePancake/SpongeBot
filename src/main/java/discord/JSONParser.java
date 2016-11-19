a4d925dec6978e9db5aba86422bb457d1107ff79
Richard <pxdp4r0x@gmail.com>
2016-11-19 11:59:59 -0800

    clean up

--- src/main/java/discord/JSONParser.java
+++ src/main/java/discord/JSONParser.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 public class JSONParser {
     public static String jsonData = "";
