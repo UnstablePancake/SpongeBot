cac9991756ed934f188114f2d5e179323aa60e12
Richard <pxdp4r0x@gmail.com>
2016-12-02 21:29:16 -0800

    initial commit

---
+++ src/main/java/commands/Luck.java
@@ -0,0 +1,26 @@
+package commands;
+
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
+import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.MissingPermissionsException;
+import sx.blah.discord.util.RateLimitException;
+
+public class Luck {
+    public static void testLuck (MessageReceivedEvent event) throws RateLimitException, DiscordException, MissingPermissionsException {
+        if (event.getMessage().toString().startsWith(">luck ")) {
+            String userGuess = event.getMessage().toString().substring(6);
+            int botGuess = (int)(Math.random() * 10);
+
+            if (Integer.parseInt(userGuess) >= 1 || Integer.parseInt(userGuess) <= 10)
+                if (userGuess.equals(Integer.toString(botGuess))) {
+                    event.getMessage().getChannel().sendMessage(event.getMessage().getAuthor().mention() + " You are lucky! The number was " + botGuess);
+                }
+                else {
+                    event.getMessage().getChannel().sendMessage(event.getMessage().getAuthor().mention() + " You are not lucky! The number was " + botGuess);
+                }
+            else {
+                event.getMessage().getChannel().sendMessage((event.getMessage().getAuthor().mention() + "Enter a number must be from 1-10"));
+            }
+        }
+    }
+}
